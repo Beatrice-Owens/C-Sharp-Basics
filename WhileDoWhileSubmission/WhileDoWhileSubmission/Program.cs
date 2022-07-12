5764f4dd167280539ad0f60a38bba784fbd93828
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2022-07-12 16:19:23 -0400

    Updated While/DoWhile Submission
    added the appropriate while statement to the assignment

--- WhileDoWhileSubmission/WhileDoWhileSubmission/Program.cs
+++ WhileDoWhileSubmission/WhileDoWhileSubmission/Program.cs
@@ -10,7 +10,61 @@ namespace WhileDoWhileSubmission
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess how old my cat is. Pick a number please."); //intro 
+            Console.WriteLine("Guess my favorite color."); //prompt
+            string favColor = Console.ReadLine(); //str var
+            favColor = favColor.ToUpper(); //making sure all input uses the same capitalization
+            bool rightColor = favColor == "PURPLE"; //looking for the input "PURPLE"
+
+            while (!rightColor) //as long as the input is NOT purple, we're doing a loop
+            {
+                switch (favColor) //using the Uppercase favColor var
+                {
+                    case "BLACK": //if black then: 
+                        Console.WriteLine("Black isn't my favorite. Try again."); //nope
+                        Console.WriteLine("Guess a color."); //guess again
+                        favColor = Console.ReadLine(); //changing var to match new input
+                        favColor = favColor.ToUpper(); //standardizing capitalization
+                        break;
+                    case "RED": //see ^^
+                        Console.WriteLine("Red isn't my favorite. Try again.");
+                        Console.WriteLine("Guess a color.");
+                        favColor = Console.ReadLine();
+                        favColor = favColor.ToUpper();
+                        break;
+                    case "BLUE":
+                        Console.WriteLine("Blue isn't my favorite. Try again.");
+                        Console.WriteLine("Guess a color.");
+                        favColor = Console.ReadLine();
+                        favColor = favColor.ToUpper();
+                        break;
+                    case "GREEN":
+                        Console.WriteLine("Green isn't my favorite. Try again.");
+                        Console.WriteLine("Guess a color.");
+                        favColor = Console.ReadLine();
+                        favColor = favColor.ToUpper();
+                        break;
+                    case "YELLOW":
+                        Console.WriteLine("Yellow isn't my favorite. Try again.");
+                        Console.WriteLine("Guess a color.");
+                        favColor = Console.ReadLine();
+                        favColor = favColor.ToUpper();
+                        break;
+                    case "PURPLE":
+                        Console.WriteLine("You're right! Purple is my favorite."); //the correct guess
+                        Console.WriteLine("Press \"Enter\" to continue."); //because of Console.ReadLine(); the app will pause after this guess. This is a hint about how to continue.
+                        rightColor = true;
+                        break;
+                    default:
+                        Console.WriteLine("That's not it. Try again.");
+                        Console.WriteLine("Guess a color.");
+                        favColor = Console.ReadLine();
+                        favColor = favColor.ToUpper();
+                        break;
+                }
+            }
+            Console.ReadLine(); //pausing after the while loop has finished for pacing and observation
+
+            Console.WriteLine("Guess how old my cat is. Pick a number please."); //intro to second question
             int number = Convert.ToInt32(Console.ReadLine()); //making the guess an int var so it can be compared to 12 in the bool age var
             bool age = number == 12; //comparison expression that returns true/false based on whether number var is 12
 
