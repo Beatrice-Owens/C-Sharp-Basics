913a9cc38230e81ed7be98ea71205f7ab4d58cb6
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2022-07-16 17:52:10 -0400

    Calling Methods Assignment
    app with separate file for created class that houses math operations to be called and executed by the main Program.cs file taking user input.

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int inputConverted = Convert.ToInt32(Console.ReadLine()); //storing input as int var
+            //declaring variables and setting to 0
+            int sum = 0;
+            int difference = 0;
+            int product = 0;
+            int quotient = 0;
+            //calling methods from UserNumber class with converted user input as passed arg
+            //storing in sum var
+            sum = UserNumber.PlusOne(inputConverted);
+            Console.WriteLine(inputConverted + " plus 1 equalts: " + sum); //writing result to console
+            //storing in difference var
+            difference = UserNumber.MinusOne(inputConverted);
+            Console.WriteLine(inputConverted + " minus 1 equals: " + difference);
+            //storing in product var
+            product = UserNumber.TimesTwo(inputConverted);
+            Console.WriteLine(inputConverted + " times 2 equals: " + product);
+            //storing in quotient var
+            quotient = UserNumber.DivideBy(inputConverted);
+            Console.WriteLine(inputConverted + " divided by 2 equals: " + quotient);
+            
+            Console.ReadLine(); //pausing console
+
+        }
+    }
+}
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/UserNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    //class UserNumber created to house math methods; public so it can be called in main Program.cs
+    public class UserNumber
+    {
+        public static int PlusOne(int num1) //public method PlusOne takes int arg from user input in Program.cs
+        {
+            int sum = num1 + 1; //simple operation
+            return sum; //return the sum as output from method
+        }
+
+        public static int MinusOne(int num1) //separate method for subtraction
+        {
+            int difference = num1 - 1; //given arg -1 stored in int var
+            return difference; //return difference as output
+        }
+
+        public static int TimesTwo(int num1) //multiplication method
+        {
+            int product = num1 * 2; // arg * 2
+            return product; //return product
+        }
+
+        public static int DivideBy(int num1) //division operation
+        {
+            int quotient = num1 / 2; //store arg / 2 in quotient
+            return quotient; //return quotient
+        }
+    }
+}
