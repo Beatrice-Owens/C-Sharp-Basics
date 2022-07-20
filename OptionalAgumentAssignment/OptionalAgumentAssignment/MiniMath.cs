ab42a298d67ce95d084a42d3f439b8ab1699fdf1
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2022-07-19 20:42:27 -0400

    Optional Argument Assignment
    Console app: takes in int from user, gives option for second int; checks if second int was input; if yes: pass both inputs to method, if no: only pass the necessary first input.

---
+++ OptionalAgumentAssignment/OptionalAgumentAssignment/MiniMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalAgumentAssignment
+{
+    public class MiniMath //make class public so Program.cs can access its contents
+    {
+        //PlusSome method takes necessary arg num1 and optional int arg
+        public static int PlusSome(int num1, int optionalNum = 1) 
+        {
+            int sum = num1 + optionalNum; //basic addition op
+            return sum; //return results
+        }
+    }
+}
---
+++ OptionalAgumentAssignment/OptionalAgumentAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalAgumentAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int number1 = Convert.ToInt32(Console.ReadLine()); //casting input to int var for math
+            Console.WriteLine("You may choose to enter a second number now. If you don't want to, just press Enter.");
+            int number2; //inst. second int var for optional param
+            //checks if the input can be cast as int and sends it out as int, sends it out as number2 if possible
+            //also needed for following if/else statements
+            bool success = int.TryParse(Console.ReadLine(), out number2); 
+
+            int sum = 0; //inst. int var "sum" to receive returned int from method PlusSome()
+
+            //if the bool success is true, input the extra optional param
+            //if not true, don't send the number2 var to the method, since it would cause errors
+            if (success)
+            {
+                sum = MiniMath.PlusSome(number1, number2); //sends both
+                Console.WriteLine("{0} plus {1} equals: {2}", number1, number2, sum); //output
+
+            }
+            else
+            {
+                sum = MiniMath.PlusSome(number1); //sends necessary param
+                Console.WriteLine("{0} plus 1 equals: {1}", number1, sum);
+
+            }
+            Console.ReadLine(); //pause for readability
+            
+
+        }
+    }
+}
