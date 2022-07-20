9a322eb8bc0a836c5c75f1bbee9cdefbe20efcb6
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2022-07-19 22:48:09 -0400

    Named Parameters Assignment
    Console app: pass 2 int to a void method; first not named; second using named param.

---
+++ MultipleNamedParams/MultipleNamedParams/IntegerIntake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleNamedParams
+{
+    public class IntegerIntake
+    {
+        //create a void method (doesn't return a value) that takes two named arg
+        public static void MultiNum(int num1, int num2)
+        {
+            //method must perform math operation on first integer
+            int sum = num1 + 1;
+            Console.WriteLine(num2); //method must simply display the second int
+            return; 
+        }
+    }
+}
---
+++ MultipleNamedParams/MultipleNamedParams/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleNamedParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inst. two int vars with values 
+            int number1 = 2;
+            int number2 = 3;
+            
+            //3. calling method a passing param two values
+            IntegerIntake.MultiNum(number1, number2);
+
+            //4. calling the method from class IntegerIntake and passing param by name
+            IntegerIntake.MultiNum(num1: number1, num2: number2);
+            //each way of calling the method should display the same value
+            Console.ReadLine();//pause for readability
+        }
+    }
+}
