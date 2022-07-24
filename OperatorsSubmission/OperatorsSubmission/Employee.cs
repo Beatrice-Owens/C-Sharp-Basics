294a4440ed2426964f6dd651c0070a4cc1d3152c
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2022-07-23 20:32:53 -0400

    Operator Overload Submission
    Console App: create class with given properties and an operator overload for == operator; instantiate objects and use operator overload to compare.

---
+++ OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    //create public class
+    public class Employee
+    {
+        //three properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //overload == operator to assess bool value of separate employee Id values
+        //returns bool value
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            //== now checks if ids are the same
+            bool equalId = employee1.Id == employee2.Id;
+            return equalId;
+        }
+        //must overload comparison operators in pairs (== and !=)
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            bool equalId = employee1.Id != employee2.Id;
+            return equalId;
+        }
+    }
+}
---
+++ OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare and instantiate new Employee class object employee1 w/all 3 properties
+            Employee employee1 = new Employee
+            {
+                Id = 101,
+                FirstName = "B",
+                LastName = "Mo"
+            };
+
+            //declare and instan. another object for comparison
+            Employee employee2 = new Employee
+            {
+                Id = 102,
+                FirstName = "D",
+                LastName = "Choe"
+            };
+
+            //write whether employee1's Id is the same as employee2's
+            //then pause
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
