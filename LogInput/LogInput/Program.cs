0644e373201d2608363f3081f6a56e78076a4c01
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2022-08-02 20:31:22 -0400

    Log Input
    Console app: receives input; writes it to external file location (log); reads & displays all info stored in that file.

---
+++ LogInput/LogInput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the number and assign it to str var b/c no math and I'm just writing & reading with it
+            Console.WriteLine("Hello. Please enter a number.");
+            string numberEntry = Console.ReadLine();
+            //write the str in numberEntry to the path listed for logging purposes
+            File.WriteAllText(@"C:\Users\point\Logs\logPractice.txt", numberEntry);
+            //create a str var that reads the txt file at target path 
+            string readLog = File.ReadAllText(@"C:\Users\point\Logs\logPractice.txt");
+            //now write info contained in the file to console
+            Console.WriteLine(readLog);
+            //pause b4 moving on
+            Console.ReadLine();
+        }
+    }
+}
