00087091ba8b655b5606de89bfb3ee0672a19cd7
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2022-08-03 23:54:17 -0400

    ChainingConstructors Assignment
    Console app: const creation, implicit var creation, chaining constructors in separate class, instan. 4 class objects, print info to console.

---
+++ ChainingConstructors/ChainingConstructors/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainingConstructors
+{
+    public class Employee
+    {
+        //chaining constructors
+        //no param for input from Program, passes default 0, default ""
+        public Employee() : this(0, "Momo")
+        {
+        }
+        //int var id param, passes id, default ""
+        public Employee(int id) : this(id, "Toph")
+        {
+        }
+        //only str var name param, passes default 0, name
+        public Employee(string name) : this(0, name)
+        {
+        }
+        //base constructor takes id and name params and assigns to props
+        public Employee(int id, string name)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+        //properties
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ ChainingConstructors/ChainingConstructors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainingConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create construct
+            const string companyName = "Bits & Bobs";
+            //implicit var creation
+            var name1 = "BMO";
+            var name2 = "Willy";
+            var id1 = 105;
+            var id2 = 113;
+            //instan. 4 Employee objects using various information input patterns
+            Employee emp1 = new Employee(), emp2 = new Employee(id1), 
+                emp3 = new Employee(name1), emp4 = new Employee(id2, name2);
+            //displaying results of different input patterns & chained constructors
+            Console.WriteLine("Employee {0} named {1} works at {2}", emp1.Id, emp1.Name, companyName);
+            Console.WriteLine("Employee {0} named {1} works at {2}", emp2.Id, emp2.Name, companyName);
+            Console.WriteLine("Employee {0} named {1} works at {2}", emp3.Id, emp3.Name, companyName);
+            Console.WriteLine("Employee {0} named {1} works at {2}", emp4.Id, emp4.Name, companyName);
+            //pause
+            Console.ReadLine();
+        }
+    }
+}
